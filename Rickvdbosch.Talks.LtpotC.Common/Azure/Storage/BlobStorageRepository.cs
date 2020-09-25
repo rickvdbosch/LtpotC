de8d147b5dfe7adbccd8d9b8585009f3f3f9af91
Rick van den Bosch <rickvdbosch@outlook.com>
2020-09-25 15:36:26 +0200

    Refactored BlobStorageRepository to new package

--- Rickvdbosch.Talks.LtpotC.Common/Azure/Storage/BlobStorageRepository.cs
+++ Rickvdbosch.Talks.LtpotC.Common/Azure/Storage/BlobStorageRepository.cs
@@ -1,79 +1,32 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 
-using Microsoft.WindowsAzure.Storage.Blob;
+using Azure.Storage.Blobs;
 
 namespace Rickvdbosch.Talks.LtpotC.Common.Azure.Storage
 {
-    public class BlobStorageRepository : BaseStorageRepository
+    public class BlobStorageRepository
     {
         #region Fields
 
-        private CloudBlobClient _blobClient;
-
-        #endregion
-
-        #region Constants
-
-        private const string FILES_CONTAINER = "files";
-        private const string IMAGES_CONTAINER = "images";
-        private const string CONTENT_CONTAINER = "content";
+        private string _connectionString;
 
         #endregion
 
         #region Constructors
 
-        public BlobStorageRepository(string connectionString) : base(connectionString)
+        public BlobStorageRepository(string connectionString)
         {
-            _blobClient = _storageAccount.CreateCloudBlobClient();
+            _connectionString = connectionString;
         }
 
         #endregion
 
-        public async Task<Stream> GetFileAsStreamAsync(string fileName)
-        {
-            var container = _blobClient.GetContainerReference(FILES_CONTAINER);
-            var blob = container.GetBlockBlobReference(fileName);
-            var stream = new MemoryStream();
-            await blob.DownloadToStreamAsync(stream);
-            stream.Seek(0, SeekOrigin.Begin);
-
-            return stream;
-        }
-
-        public async Task<string> GetFileContentAsync(string fileName)
-        {
-            var container = _blobClient.GetContainerReference(CONTENT_CONTAINER);
-            var blob = container.GetBlockBlobReference(fileName);
-
-            return await blob.DownloadTextAsync();
-        }
-
-        public async Task DeleteImage(string filename)
-        {
-            var container = _blobClient.GetContainerReference(IMAGES_CONTAINER);
-            var blob = container.GetBlockBlobReference(filename);
-
-            if (await blob.ExistsAsync())
-            {
-                await blob.DeleteAsync();
-            }
-        }
-
         public async Task AddFileAsync(string containerName, string filename, Stream file)
         {
-            var container = _blobClient.GetContainerReference(containerName);
-            await container.CreateIfNotExistsAsync();
-            var blob = container.GetBlockBlobReference(filename);
+            var blobClient = new BlobClient(_connectionString, containerName, filename);
 
-            if (await blob.ExistsAsync())
-            {
-                // TODO: handle this?
-            }
-            else
-            {
-                await blob.UploadFromStreamAsync(file);
-            }
+            await blobClient.UploadAsync(file);
         }
     }
 }
